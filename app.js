fd2e732f17bd4882cbcc009850fab74c4b362ab0
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2021-02-27 17:15:13 -0300

    starter_point

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const userRouter = require('./src/routes/users.router')
+
 app.use(express.json())
-app.use(`${process.env.BASE_URL}/usuarios`, require('./src/routes/users.router'))
+
+app.use(`${process.env.BASE_URL}/usuarios`, userRouter)
 
 
 module.exports = app
