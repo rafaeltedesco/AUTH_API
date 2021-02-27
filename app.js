4f648b9d70b3e48534eb86f8550a0d366fa1b950
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2021-02-27 17:42:40 -0300

    customize error handlers v1

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const AppError = require('./src/utils/appError')
+const globalErrorHandler = require('./src/controllers/error.controller')
+
+const userRouter = require('./src/routes/users.router')
+
+
 app.use(express.json())
-app.use(`${process.env.BASE_URL}/usuarios`, require('./src/routes/users.router'))
+app.use(`${process.env.BASE_URL}/usuarios`, userRouter)
+
+
+app.all('*', (req, res, next)=> {
+  next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404))
+})
+
+
+app.use(globalErrorHandler)
 
 
 module.exports = app
